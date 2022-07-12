4f85ede983b73ee43c989a630d6b28342581a35c
Artur <82472015+benyaar@users.noreply.github.com>
2022-07-12 22:53:10 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.get('/', (req:Request, res:Response) => {
-    let helloWorld = 'Hello World 55!';
+    let helloWorld = 'Запомни! Есть первое место и остальные! АУФ';
     res.send(helloWorld)
 })
 
